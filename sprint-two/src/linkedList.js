9ba8a83122c111acbbd5b369032bc9657a044a96
Max <maxmillianrwoods@gmail.com>
2019-05-30 16:05:56 -0700

    linked list add and remove working

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,29 +1,42 @@
-var LinkedList = function() {
+var LinkedList = function () {
   var list = {};
   list.head = null;
   list.tail = null;
-
-  list.addToTail = function(value) {
+  list.size = 0;
+  list.addToTail = function (value) {
+    var node = Node(value);
+    if (list.tail == null) {
+      list.tail = node;
+      list.head = node;
+    } else {
+      current = list.tail;
+      current.next = node;
+      list.tail = current.next;
+    }
+    list.size++;
   };
 
-  list.removeHead = function() {
+  list.removeHead = function () {
+    var prev = list.head.value;
+    var current = list.head;
+    if (list.size === 2) {
+      current = list.tail;
+    }
+    list.head = current;
+    list.size--;
+    return prev;
   };
 
-  list.contains = function(target) {
-  };
-
+  list.contains = function (target) {};
   return list;
 };
 
-var Node = function(value) {
+var Node = function (value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
