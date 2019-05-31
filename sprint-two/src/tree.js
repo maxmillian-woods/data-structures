00933de0a79b147b243d4e8351c0f3defd352779
maxmillian-woods <maxmillianrwoods@gmail.com>
2019-05-31 16:01:39 -0700

    with this commit, tree.js in functional shared instantiation style passes all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,55 @@
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  
+  
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(value) { 
+  let newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    }
+    if (this.children[i].children.length !== 0) {
+      if (this.children[i].contains(target) === true) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
+let printObject = function(object) {
+  console.log(JSON.stringify(object));
+};
+
+// let myTree = Tree();
+
+//  myTree.addChild('Max');
+// printObject(myTree.children);
+// myTree.addChild('Patrick');
+// printObject(myTree.children);
+// console.log(myTree.contains('Max'));
+
+
+// myTree.addChild(5);
+// myTree.addChild(6);
+// myTree.children[0].addChild(7);
+// myTree.children[1].addChild(8);
+// printObject(myTree.children);
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
